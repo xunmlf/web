eba3c82707c7a31984ff2ffde0d6c5a66e2d04a2
xunmlf <2407942305@qq.com>
2020-12-17 18:48:03 +0800

    2020/12/17

---
+++ JavaScript/js/07.上课.js
@@ -0,0 +1,10 @@
+function orang(){
+    var array=[123,45,45,78,85];
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+    console.log(element);
+    }
+}
+
+orang();
+
